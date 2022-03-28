fc01b7f46eb052411e35c77a131a4cac3fc399c3
Aline Bury <alinebury@gmail.com>
2022-03-28 15:06:36 -0300

    Test NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Requisito 4 - Teste componente NotFound', () => {
+  test('Teste se a página contém headind h2 com texto Page requested not found ', () => {
+    render(<NotFound />);
+
+    const title = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji', level: 2 });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra a imagem da variável URL', () => {
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    render(<NotFound />);
+
+    const image = screen.getAllByRole('img')[1];
+    expect(image.src).toBe(URL);
+  });
+});
