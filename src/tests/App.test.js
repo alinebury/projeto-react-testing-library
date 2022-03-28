56ffc2721239bc899da07bcfde39126d44c11974
Aline Bury <alinebury@gmail.com>
2022-03-28 14:25:28 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Requisito 1 - Teste o componente App', () => {
+  test('O primeiro link deve possuir o texto Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+
+  test('O segundo link deve possuir o texto About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    userEvent.click(aboutLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+
+  test('O tercceiro link deve possuir o texto Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoriteLink).toBeInTheDocument();
+
+    userEvent.click(favoriteLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Teste  a página Not Found ao entrar em uma URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/not-found');
+
+    const pag = screen.getByText('Page requested not found');
+    expect(pag).toBeInTheDocument();
+  });
+});
